184fb81ed38471cf25a44a1451c5e3eb4027601e
tanaka.satoshi <tanaka.satoshi@a-tm.co.jp>
2024-04-02 05:44:59 +0900

    update supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
+import { Tables } from '@/types/supabase'
 
 export default async function Page() {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore)
 
-  const { data: todos } = await supabase.from('todos').select()
+  const personsQuery = supabase.from('persons').select()
 
-  return <ul>{todos?.map((todo) => <li>{todo}</li>)}</ul>
+  const { data, error } = await personsQuery
+  if (error) throw error
+  const persons: Tables<'persons'>[] = data
+
+  return (
+    <ul>
+      {persons?.map(({ id, first_name, last_name }) => (
+        <li key={id}>
+          {id}: {first_name}
+          {last_name}
+        </li>
+      ))}
+    </ul>
+  )
 }
